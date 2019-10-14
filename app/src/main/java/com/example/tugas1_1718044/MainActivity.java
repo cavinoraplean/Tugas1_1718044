209b07b060f29d2245c13d6a2846ed7256ecfcce
cavinoraplean <cavinyohanesoraplean@gmail.com>
2019-10-14 20:45:16 +0700

    Tugas 1

---
+++ app/src/main/java/com/example/tugas1_1718044/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tugas1_1718044;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
